af935b6c55730e35407872d89080bb91f58e3166
Jennifer Mitchell <jenmitchell@fullerton.edu>
2025-05-28 11:19:09 -0700

    added missing scrolling to top on Contributors and Tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import Gallery from './components/Gallery';
@@ -9,9 +9,21 @@ import Tags from './components/Tags';
 import Insights from './components/Insights';
 import Contributors from './components/Contributors';
 
+// Scroll to top component
+function ScrollToTop() {
+  const location = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <a href="#main-content" className="skip-link">
         Skip to main content
       </a>
@@ -25,7 +37,7 @@ function App() {
             <Route path="/tags" element={<Tags />} />
             <Route path="/insights" element={<Insights />} />
             <Route path="/contributors" element={<Contributors />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
+            <Route path="*" element={<Navigate to="/\" replace />} />
           </Routes>
         </main>
       </Layout>
